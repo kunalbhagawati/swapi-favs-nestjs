54f7cec0cdc4562957ac443ff0c9e48c6724b657
knl.bhg@gmail.com <knl.bhg@gmail.com>
2023-04-20 00:51:45 +0530

    Add sorting to final movie list.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import MoviesService from "./movies.service";
 import { UserId } from "../lib/user.decorator";
+import { prop, sortBy } from "ramda";
 
 @Controller("movies")
 export default class MoviesController {
-  constructor(private readonly moviesService: MoviesService) {
-  }
+  constructor(private readonly moviesService: MoviesService) {}
 
   @Get()
   async getMovies(@UserId() userId: string, @Query("search") search: string) {
-    return this.moviesService.getAll(userId, `${search}`);
+    return sortBy(
+      prop("title"),
+      await this.moviesService.getAll(userId, search),
+    );
   }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -11,8 +11,7 @@ export default class MoviesService {
   constructor(
     private readonly moviesRepo: MoviesRepository,
     private readonly favoritesService: FavoritesService,
-  ) {
-  }
+  ) {}
 
   async getAll(userId?: string, search?: string): Promise<MovieDTO[]> {
     const movies = await this.moviesRepo.getAll();
