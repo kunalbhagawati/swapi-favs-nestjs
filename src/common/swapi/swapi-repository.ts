2c92a2293840b47586e8913efcba33cded8e6c26
knl.bhg@gmail.com <knl.bhg@gmail.com>
2023-04-20 19:50:14 +0530

    Use write-through cache for performance.

--- src/common/swapi/swapi-repository.ts
+++ src/common/swapi/swapi-repository.ts
@@ -6,6 +6,9 @@ import { ConfigService } from "@nestjs/config";
 import { serializeFilm, serializePlanet } from "./swapi-serializer";
 import { Movie } from "../../movies/movies.types";
 import { Planet } from "../../planets/planets.types";
+import { InjectRedis } from "@liaoliaots/nestjs-redis";
+import Redis from "ioredis";
+import { isEmpty, isNil, map, reduce, values } from "ramda";
 
 @Injectable()
 export default class SwapiRepository {
@@ -14,7 +17,21 @@ export default class SwapiRepository {
     [ResourceType.Planet]: "planets",
   };
 
-  constructor(private readonly nestConfig: ConfigService) {}
+  private static readonly resourceTypeRedisKeyMap = {
+    [ResourceType.Movie]: "resources::films",
+    [ResourceType.Planet]: "resources::planets",
+  };
+
+  private static readonly resourceTypeSerializerFunction = {
+    [ResourceType.Movie]: serializeFilm,
+    [ResourceType.Planet]: serializePlanet,
+  };
+
+  constructor(
+    @InjectRedis() private readonly redis: Redis,
+    private readonly nestConfig: ConfigService,
+  ) {
+  }
 
   get config(): SwapiConfig {
     return this.nestConfig.getOrThrow<SwapiConfig>("swapi");
@@ -26,11 +43,47 @@ export default class SwapiRepository {
   urlPath = (resourceType: ResourceType): string =>
     `${this.config.baseUrl}/${this.resourcePath(resourceType)}`;
 
-  getAllFilms = async () =>
-    this.getAll<Movie>(ResourceType.Movie, serializeFilm);
+  getAllFilms = async () => this.fetchUsingCache<Movie>(ResourceType.Movie);
 
-  getAllPlanets = async () =>
-    this.getAll<Planet>(ResourceType.Planet, serializePlanet);
+  getAllPlanets = async () => this.fetchUsingCache<Planet>(ResourceType.Planet);
+
+  private fetchUsingCache = async <T extends Movie | Planet>(
+    resourceType: ResourceType,
+  ): Promise<T[]> => {
+    const res = await this.redis.hgetall(this.redisKey(resourceType));
+
+    if (!isNil(res) && !isEmpty(res)) {
+      return map(this.decodeFromRedis, values(res)) as T[];
+    }
+
+    return this.fetchFromAPIAndWriteThrough<T>(resourceType);
+  };
+
+  private fetchFromAPIAndWriteThrough = async <T extends Movie | Planet>(
+    resourceType: ResourceType,
+  ) => {
+    const res = await this.getAll<T>(
+      resourceType,
+      SwapiRepository.resourceTypeSerializerFunction[resourceType],
+    );
+
+    await this.redis.hmset(
+      this.redisKey(resourceType),
+      reduce(
+        (acc, value: T) => ({ ...acc, [value.url]: this.encodeToRedis(value) }),
+        {},
+        res,
+      ),
+    );
+    this.redis.expire(this.redisKey(resourceType), 60 * 60);
+    return res;
+  };
+
+  private redisKey = (resourceType: ResourceType): string =>
+    SwapiRepository.resourceTypeRedisKeyMap[resourceType];
+
+  private encodeToRedis = (r: unknown): string => JSON.stringify(r);
+  private decodeFromRedis = (r: string): unknown => JSON.parse(r);
 
   private getAll = async <T extends Movie | Planet>(
     resourceType: ResourceType,
